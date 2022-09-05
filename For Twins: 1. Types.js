43fe3e58160a84430f6eb4cfde199f39eb4d2fc4
TheSunnyDev <109054640+TheSunnyDev@users.noreply.github.com>
2022-09-05 13:46:06 -0700

    Pushing a new kata solution

---
+++ For Twins: 1. Types.js
@@ -0,0 +1,13 @@
+// Task:
+
+// Write a function that will accept two parameters: variable and type and check if type of variable is matching type. Return true if types match or false if not.
+
+// Examples:
+
+// 42, "number"   --> true
+// "42", "number" --> false
+
+
+
+Solution:
+typeValidation = (variable, type) => typeof variable == type
