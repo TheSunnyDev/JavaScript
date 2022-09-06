0bc21069d8541d8c3c7c95023324cb429abd340e
TheSunnyDev <109054640+TheSunnyDev@users.noreply.github.com>
2022-09-06 09:22:39 -0700

    Update Messi Goals Function.js
    commented out the description

--- Messi Goals Function.js
+++ Messi Goals Function.js
@@ -1,11 +1,11 @@
-Messi is a soccer player with goals in three leagues:
+// Messi is a soccer player with goals in three leagues:
 
-LaLiga
-Copa del Rey
-Champions
-Complete the function to return his total number of goals in all three leagues.
+// LaLiga
+// Copa del Rey
+// Champions
+// Complete the function to return his total number of goals in all three leagues.
 
-Note: the input will always be valid.
+// Note: the input will always be valid.
 
 For example:
 
