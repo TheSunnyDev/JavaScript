c7b12ebf57a2b4ab50b73424dc052c2a377944b7
thesunnydev <109054640+TheSunnyDev@users.noreply.github.com>
2023-01-06 17:41:34 -0800

    add alternative solution

--- find-the-remainder.js
+++ find-the-remainder.js
@@ -1,9 +1,17 @@
-Write a function that accepts two integers and returns the remainder of dividing the larger value by the smaller value.
+// Write a function that accepts two integers and returns the remainder of dividing the larger value by the smaller value.
 
-Division by zero should return NaN.
+// Division by zero should return NaN.
 
-Solution:
+// Solution:
 
 function remainder(n, m){
-    return n > m ? n % m : m % n
+    return n > m ? n % m : m % n;
    }
+
+// Additional Solution:
+function remainder(a, b){
+    if(b > a)
+      return b % a;
+    else
+      return a % b;
+  }
