17c06da150c2cc32e6d47f6cf9989f7de4e40363
thesunnydev <109054640+TheSunnyDev@users.noreply.github.com>
2023-01-23 16:45:58 -0800

    add new solution

---
+++ duck-duck-goose.js
@@ -0,0 +1,4 @@
+// Task: Given an array of Player objects (an array of associative arrays in PHP) and an index (1-based), return the name of the chosen Player(name is a property of Player objects, e.g Player.name)
+
+Solution:
+duckDuckGoose = (players, goose) => players[(goose - 1) % players.length].name
