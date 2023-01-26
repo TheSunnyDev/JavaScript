6b9e9e6fc1f8ab004ae698d2b97b284ee27c729c
thesunnydev <109054640+TheSunnyDev@users.noreply.github.com>
2023-01-25 16:18:58 -0800

    add new solution

---
+++ apple-turnover.js
@@ -0,0 +1,11 @@
+// Your job is simple, if x squared is more than 1000, return It's hotter than the sun!!, else, return Help yourself to a honeycomb Yorkie for the glovebox.
+
+Solution:
+function apple(x){
+    x = x ** 2
+    if (x > 1000) {
+      return 'It\'s hotter than the sun!!'
+    } else {
+      return 'Help yourself to a honeycomb Yorkie for the glovebox.'
+    }
+  }
