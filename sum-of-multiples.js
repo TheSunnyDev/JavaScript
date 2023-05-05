9f2a3002cd4da9944c52918c96d74dca3164f682
thesunnydev <109054640+TheSunnyDev@users.noreply.github.com>
2023-05-04 20:56:41 -0700

    add new solution

---
+++ sum-of-multiples.js
@@ -0,0 +1,18 @@
+// Find the sum of all multiples of n below m
+
+// Keep in Mind
+
+// n and m are natural numbers (positive integers)
+// m is excluded from the multiples
+
+// Solution:
+
+function sumMul(n,m){
+    if(n >= m) return "INVALID";
+    
+    var sum = 0;
+      for (var i = n; i < m; i+=n){
+        sum += i;
+      }
+    return sum;
+  }
