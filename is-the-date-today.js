7d270a72968e515b60be093de23cb19be6dd22e5
thesunnydev <109054640+TheSunnyDev@users.noreply.github.com>
2023-05-15 20:02:33 -0700

    add new solution

---
+++ is-the-date-today.js
@@ -0,0 +1,10 @@
+// Write a simple function that takes a Date as a parameter and returns a Boolean representing whether the date is today or not.
+
+// Make sure that your function does not return a false positive by only checking the day.
+
+// Solution:
+
+function isToday(date) {
+    const today = new Date() 
+    return today.setHours(0,0,0,0) == date.setHours(0,0,0,0);
+  }
