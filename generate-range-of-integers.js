8500a4d9f75c4c1b450d371ffc5eedc3d092a7f9
thesunnydev <109054640+TheSunnyDev@users.noreply.github.com>
2024-01-23 21:42:01 -0800

    Add new kata and solution

---
+++ generate-range-of-integers.js
@@ -0,0 +1,10 @@
+// Implement the function generateRange which takes three arguments (start, stop, step) and returns the range of integers from start to stop (inclusive) in increments of step.
+
+// Solution:
+function generateRange(min, max, step){
+    let arr = [];
+    for (let i = min; i <= max; i+=step){
+      arr.push(i)
+    }
+    return arr;
+  };
